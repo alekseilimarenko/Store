4621f24f0e71231c4cc876abdab3fbdcdbd5944b
Алексей <vega74@ua.fm>
2017-09-27 16:57:23 +0300

    27.09.2017

--- Store/Models/Magazin.cs
+++ Store/Models/Magazin.cs
@@ -22,3 +22,64 @@ namespace Store.Models
         public DateTime intervalto { get; set; }
     }
 }
+
+
+
+//$(function() {
+//    $('#campId').on("change",
+//        function() {
+//            //console.log("campid change called");
+
+//            //получение значения аттрибута value элемента seasonInstances
+//            var seasonInstanceId = instances.options[instances.selectedIndex].getAttribute('value');
+
+//            var route = '/admin/DocOrder/_PartChangeLapDocPriceItems?campId=' + camp.value + '&seasonInstanceId=' + seasonInstanceId;
+//            //console.log(route);
+
+//            $('#priceDocIds').load(route,
+//                function( /*response, status, xhr*/) {
+//                    $('#priceDocIds').change();
+//                });
+//        }
+//    );
+//    //console.log("campid change done");
+//});
+
+//$(function() {
+//    $('#priceDocIds').on("change",
+//        function() {
+//            //console.log("doc change called");
+//            var priceDocId = $(this).val();
+//            $('#priceDocIds').change = reloadPricePosDropDown(priceDocId);
+//            //console.log("doc change done");
+//        }
+//    );
+//});
+
+//function reloadPricePosDropDown(docId) {
+//    //console.log("reloadPricePosDropDown called docId=" + docId);
+//    var route = '/admin/DocOrder/_PartChangeLapDocPricePosItems?docCampSeasonPriceId=' + docId;
+//    //console.log(route);
+//    $('#docCampSeasonPricePosId').load(route);
+//    //console.log("reloadPricePosDropDown done");
+//}
+
+//$(document).ready(function () {
+//    //console.log("document");
+
+//    $('form').on('submit', function (e) {
+//        e.preventDefault();
+//        console.log("submit");
+
+//        var pricePosId = $('#docCampSeasonPricePosId').val();
+//        var order = document.getElementById('orderId');
+
+//        var route = '/admin/DocOrder/ChangeLapConfirmed?' +
+//            '&pricePosId=' + pricePosId +
+//            '&id=' + order.value;
+
+//        console.log(route);
+
+//        document.location = route;
+//    });
+//});
