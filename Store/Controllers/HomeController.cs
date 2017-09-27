03dd1619a52a3ec886ebd3bfb10f6d8eb8c82a32
voyager <voyager>
2017-09-27 21:13:38 +0300

    add linq

--- Store/Controllers/HomeController.cs
+++ Store/Controllers/HomeController.cs
@@ -29,6 +29,18 @@ namespace Store.Controllers
         [HttpPost]
         public ActionResult Index(int id)
         {
+            var results = from c in db.Companies
+                          join cn in db.Countries on c.CountryID equals cn.ID
+                          join ct in db.Cities on c.CityID equals ct.ID
+                          join sect in db.Sectors on c.SectorID equals sect.ID
+                          where (c.CountryID == cn.ID) && (c.CityID == ct.ID) && (c.SectorID == company.SectorID) && (company.SectorID == sect.ID)
+                          select new { country = cn.Name, city = ct.Name, c.ID, c.Name, c.Address1, c.Address2, c.Address3, c.CountryID, c.CityID, c.Region, c.PostCode, c.Telephone, c.Website, c.SectorID, Status = (ContactStatus)c.StatusID, sector = sect.Name };
+
+
+            return results.ToList();
+
+
+
             var str = id;
 
             return View();
